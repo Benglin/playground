ad7fac862701dd503e81839ae66d7657a2531876
pixelhypercube <6thetce@gmail.com>
2022-07-09 15:08:34 +0800

    Attempted to do the suggested changes + other improvements

--- pages/components/IconLabel.tsx
+++ pages/components/IconLabel.tsx
@@ -1,19 +1,58 @@
 // IconLabel form fields used for headers
 
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope,faSchool,faCheckCircle,faLock,faBoltLightning,faClock,faCalendar,faClockRotateLeft,faIdBadge,faRoad } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faEnvelope,
+    faSchool,
+    faBoltLightning,
+    faClock,
+    faCalendar,
+    faCheckCircle
+} from "@fortawesome/free-solid-svg-icons";
+
+import { Box } from "@mui/material";
 
 import styles from "../../styles/iconLabel.module.css";
 
-export default function FormField({title,value,icon,verified,fontSize}: any): JSX.Element {
-    return (
-        <div style={{display:"flex",flexDirection:"row",alignItems:"center"}}>
-            <FontAwesomeIcon icon={icon}></FontAwesomeIcon>
-            <p style={{fontSize}} className={styles.secondaryProfileDesc}>{title}</p>
-            <p style={{fontSize}} className={styles.secondaryProfileDesc}>{value}</p>
-            <FontAwesomeIcon style={{marginLeft:"10px",color:"#2897ff",display:verified ? "block" : "none"}} icon={faCheckCircle} title={"Verified"}></FontAwesomeIcon>
-        </div>
-    )
+export default function FormField({
+  title,
+  value,
+  icon,
+  verified,
+  fontSize,
+}: any): JSX.Element {
+  return (
+    <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+      <FontAwesomeIcon icon={icon}></FontAwesomeIcon>
+      <Box
+        component="p"
+        sx={{
+          fontSize,
+          margin: "5px 0px 5px 5px",
+        }}
+      >
+        {title}
+      </Box>
+      <Box
+        component="p"
+        sx={{
+          fontSize,
+          margin: "5px 0px 5px 5px",
+        }}
+      >
+        {value}
+      </Box>
+      <FontAwesomeIcon
+        style={{
+          marginLeft: "10px",
+          color: "#2897ff",
+          display: verified ? "block" : "none",
+        }}
+        icon={faCheckCircle}
+        title={"Verified"}
+      ></FontAwesomeIcon>
+    </Box>
+  );
 }
